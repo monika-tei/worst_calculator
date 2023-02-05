03b4540c387e561e5c824f429fe531f61a8586a2
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-05 17:16:38 +0100

    testing if operators are working

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 "use strict";
 
+let result;
 let firstNumber = document.getElementById("firstnumber");
 let secondNumber = document.getElementById("secondnumber");
-let operator = document.getElementById("operator").value;
+let operator = document.querySelector("#operator").value;
 
 const calc_btn = document.getElementById("calculate");
 const resultList = document.getElementById("results");
 const resultListElement = document.querySelector("#results li");
 
 // load screen
-// Activate event listener for when a user clicks "calculate button"
+// activate event listener for when a user clicks "calculate button"
 window.addEventListener("load", () => {
   calc_btn.addEventListener("click", calculateNow);
-  console.log(operator);
 });
 
-// calc_btn.addEventListener("click", calculateNow);
-
 // Calculate
+// 4 options to do math: add, subtract, multiply, divide;
 function calculateNow() {
-  console.log("calculating...", operator);
+  operator = document.querySelector("#operator").value;
+  console.log(operator);
+
+  if (operator === "add") {
+    result = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+  } else if (operator === "sub") {
+    result = parseInt(firstNumber.value) - parseInt(secondNumber.value);
+  } else if (operator === "mul") {
+    result = parseInt(firstNumber.value) * parseInt(secondNumber.value);
+  } else {
+    result = parseInt(firstNumber.value) / parseInt(secondNumber.value);
+  }
+  console.log(result);
+  //   firstNumber.value = result;
 }
-//Read first number
-//Read second number
-// Read operator
+
+//Display result in the list above
 
 // Clear
 

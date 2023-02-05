d2e28eded5f6828e19144d73e1404a23fca451de
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-05 16:26:40 +0100

    initializing repository

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let firstNumber = document.getElementById("firstnumber");
+let secondNumber = document.getElementById("secondnumber");
+
+const operator = document.getElementById("operator");
+const calc_btn = document.getElementById("calculate");
+
+// Read first number
+//Read second number
+// Read operator
+// Calculate
+// Clear
+
+//optional: do rounding
